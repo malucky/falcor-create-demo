96e9fd8dde21004b3da2d90d9a51cdbec9e46eca
Ray Ma <rhm262@gmail.com>
2015-11-12 14:10:49 -0800

    change events to products

--- app.js
+++ app.js
@@ -23,58 +23,58 @@ pieces.
 
 /* === Step 1 === */
 
-// We can prime the model cache with a new falcor.Model
+// // We can prime the model cache with a new falcor.Model
 // var model = new falcor.Model({
 //   cache: {
-//     events: [
+//     products: [
 //       {
-//         name: "ng-conf",
-//         description: "The world's best Angular Conference",
-//         stuff: "oh hey stuff",
-//         location: { city: "Salt Lake City", state: "Utah" }
+//         id: 24,
+//         name: 'Tomorrow is today, Red printed scarf',
+//         price: '$15.00',
+//         categories: [
+//           {
+//             id: 1,
+//             name: 'accessories'
+//           }
+//         ],
 //       },
 //       {
-//         name: "React Rally",
-//         description: "Conference focusing on Facebook's React",
-//         location: { city: "Salt Lake City", state: "Utah" }
-//       },
-//       {
-//         name: "ng-Vegas",
-//         description: "Two days jam-packed with Angular goodness with a focus on Angular 2",
-//         location: { city: "Las Vegas", state: "Nevada" }
-//       },
-//       {
-//         name: "Midwest JS",
-//         description: "Midwest JS is a premier technology conference focused on the JavaScript ecosystem.",
-//         location: { city: "Minneapolis", state: "Minnesota" }
-//       },
-//       {
-//         name: "NodeConf",
-//         description: "NodeConf is the longest running community driven conference for the Node community.",
-//         location: { city: "Walker Creek Ranch", state: "California" }
+//         id: 25,
+//         name: 'Chanel the Cheetah',
+//         price: '$500.00',
+//         categories: [
+//           {
+//             id: 1,
+//             name: 'accessories'
+//           },
+//           {
+//             id: 2,
+//             name: 'bags'
+//           }
+//         ],
 //       }
 //     ]
-//   }  
+//   }
 // });
 
 // model
-//   We want the name and description values for the first three items
-//   from the data model
-//   .get(["events", {from: 0, to: 2}, ["name", "description"]])
-//   To get the values on the "location" object, we need to pass the paths for the
-//   keys on that object
-//   .get(["events", {from: 0, to: 2}, ["name", "description", "location"],["city", "state"]])
+//   // We want the name and price values for the first two products
+//   // from the data model
+//   // FIX ME
+//   // .get(["products", {from: 0, to: 1}, ["name", "price"]], ["products", {from: 0, to: 1}, "categories", 0, "name"])
+//   .get(["products", {from: 0, to: 2}, ["name", "price", "categories"]])
 //   .then(function(response) {
 //     document.getElementById("event-data").innerHTML = JSON.stringify(response, null, 2);
 //   });
 
 // model
-      // We set the value of the first occurrence of Utah to UT
-//   .set(falcor.pathValue(["events", 0, "location", "state"], 'UT'))
+//   // We set the value of the first occurrence of accesories to accessories
+//   .set(falcor.pathValue(["products", 0, "categories", 0, "name"], 'changed'))
 //   .then(function(response) {
 //     model
-          // What we find afterwards is that the value gets changed in one location, but not both.
-//       .get(["events", {from: 0, to: 2}, ["name", "description", "location"],["city", "state"]])
+//       // What we find afterwards is that the value gets changed in one location, but not both.
+//       // FIX ME
+//       .get(["products", {from: 0, to: 1}, ["name", "price"]], ["products", {from: 0, to: 1}, "categories", 0, "name"])
 //       .then(function(response) {
 //         document.getElementById('event-data').innerHTML = JSON.stringify(response, null, 2);
 //       });    
@@ -83,54 +83,36 @@ pieces.
 
 /* === Step 2 === */
 
-// We can use the shorthand for references with a variable
+// // We can use the shorthand for references with a variable
 // var $ref = falcor.Model.ref;
 
 // var model = new falcor.Model({
 //   cache: {
-//     locationsById: {
+//     categoriesById: {
+//       0: {
+//         name: 'accesories'
+//       },
 //       1: {
-//         city: "Salt Lake City",
-//         state: "Utah"
-//       },
-//       2: {
-//         city: "Las Vegas",
-//         state: "Nevada"
-//       },
-//       3: {
-//         city: "Minneapolis",
-//         state: "Minnesota"
-//       },
-//       4: {
-//         city: "Walker Creek Ranch",
-//         state: "California"
+//         name: 'bags'
 //       }
 //     },
-//     events: [
+//     products: [
 //       {
-//         name: "ng-conf",
-//         description: "The world's best Angular Conference",
-//         location: $ref('locationsById[1]')
+//         id: 24,
+//         name: 'Tomorrow is today, Red printed scarf',
+//         price: '$15.00',
+//         categories: [
+//           $ref('categoriesById[1]')
+//         ],
 //       },
 //       {
-//         name: "React Rally",
-//         description: "Conference focusing on Facebook's React",
-//         location: $ref('locationsById[1]')
-//       },
-//       {
-//         name: "ng-Vegas",
-//         description: "Two days jam-packed with Angular goodness with a focus on Angular 2",
-//         location: $ref('locationsById[2]')
-//       },
-//       {
-//         name: "Midwest JS",
-//         description: "Midwest JS is a premier technology conference focused on the JavaScript ecosystem.",
-//         location: $ref('locationsById[3]')
-//       },
-//       {
-//         name: "NodeConf",
-//         description: "NodeConf is the longest running community driven conference for the Node community.",
-//         location: $ref('locationsById[4]')
+//         id: 25,
+//         name: 'Chanel the Cheetah',
+//         price: '$500.00',
+//         categories: [
+//           $ref('categoriesById[1]'),
+//           $ref('categoriesById[2]')
+//         ],
 //       }
 //     ]
 //   }  
@@ -138,11 +120,12 @@ pieces.
 
 
 // model
-      // Now when we set Utah to UT for the first occurrence, it is changed everywhere else 
-//   .set(falcor.pathValue(["events", 0, "location", "state"], 'UT'))
+//   // Now when we rename the a category, it is reflected in both locations
+//   .set(falcor.pathValue(["products", 0, "categories", 0, "name"], 'changed'))
 //   .then(function(response) {
 //     model
-//       .get(["events", {from: 0, to: 2}, ["name", "description", "location"],["city", "state"]])
+//       // What we find afterwards is that the value gets changed in one location, but not both.
+//       .get(["products", {from: 0, to: 1}, ["name", "price"]], ["products", {from: 0, to: 1}, "categories", 0, "name"])
 //       .then(function(response) {
 //         document.getElementById('event-data').innerHTML = JSON.stringify(response, null, 2);
 //       });    
@@ -151,8 +134,8 @@ pieces.
 
 /* === Step 3 === */
 
-// We can set the model to have a data source that is retrieved from the backend
-// over HTTP by setting the soure to be a falcor.HttpDataSource.
+// // We can set the model to have a data source that is retrieved from the backend
+// // over HTTP by setting the soure to be a falcor.HttpDataSource.
 var model = new falcor.Model({source: new falcor.HttpDataSource('/model.json')});
 
 model
@@ -160,11 +143,3 @@ model
   .then(function(response) {
     document.getElementById('event-data').innerHTML = JSON.stringify(response, null, 2);
   });
-
-// Search example - we pass "Midwest JS" which will be looked up
-// in the events data on the server and sent back if it exists
-// model
-//   .get(["events", "byName", ["Midwest JS"], ['description']])
-//   .then(function(response) {
-//     document.getElementById('event-data').innerHTML = JSON.stringify(response, null, 2);
-//   });  
